170bcbbc19e1dc5ab0e55ab579615d2362543a62
Murat YILDIRIM <46280331+murtimur@users.noreply.github.com>
2025-07-29 21:04:14 +0300

    Update

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -8,45 +8,10 @@ import { Observable } from 'rxjs';
 export class SignupService {
 
   constructor(private http: HttpClient) { }
-
-  getOzelKodlar():Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.0/ozelkodlar');
-  }
-
-  getKasalar(): Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.0/kasalar');
-  }
-
-  getSubeler(): Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.0/subeler');
-  }
-
-  getAllWinkaKullanicilari(): Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.0/kullanicilar');
-  }
-
-  getAllWindowsYazicilari(): Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.0/yazicilar');
-  }
-
-  getAllPersoneller():Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.0/personeller');
-  }
-
+  
   userCreate(user: any):Observable<any>{
     const url = JSON.parse(localStorage.getItem('setting'));
     return this.http.post(url + '/api/1.0.0/users', user);
-  }
-
-  getFiyatTipleri():Observable<any> {
-    const url = JSON.parse(localStorage.getItem('setting'));
-    return this.http.get(url + '/api/1.0.4/fiyatadlari');
   }
 
 }
